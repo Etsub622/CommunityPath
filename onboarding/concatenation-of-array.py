a0fd2781ba8a7a4fbe5d388d2279152a7ab6f002
Lelina622 <115922945+Etsub622@users.noreply.github.com>
2023-12-05 00:47:20 -0800

    Added solution for Concatenation of Array

---
+++ onboarding/concatenation-of-array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        output=[]
+        for i in nums:
+            output.append(i)
+        return output+output    
+        
